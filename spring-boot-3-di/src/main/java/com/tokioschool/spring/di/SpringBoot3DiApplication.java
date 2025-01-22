65804642e4fa2d21035c20bb4c797cb33e9fa9f6
Andrés Ruiz Peñuela <andres.rpenuela@babelgroup.com>
2025-01-22 17:51:36 +0100

    [sts6-di] created project with architecture basic and connection to h2 as bbbdd

---
+++ spring-boot-3-di/src/main/java/com/tokioschool/spring/di/SpringBoot3DiApplication.java
@@ -0,0 +1,13 @@
+package com.tokioschool.spring.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3DiApplication.class, args);
+	}
+
+}
